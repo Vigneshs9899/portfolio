d969bb8d066baef1d6b18bc4118f737969445598
Vignesh <142200812+Vigneshs9899@users.noreply.github.com>
2025-05-25 16:37:03 +0530

    Update layout.js with meta tags and scripts

--- app/layout.js
+++ app/layout.js
@@ -20,9 +20,26 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-smooth">
     <head>
+      <meta name="keyword" content="vignesh,developer,websitedeveloper,vigneshdeveloper,website,vigneshprofile">
       <meta name="author" content="vignesh"/>
       <meta name="description" content="vignesh – a Frontend Developer specializing in React, Tailwind CSS, and modern web apps." />
       <meta name="robots" content="index, follow" />
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify({
+        "@context": "https://schema.org",
+        "@type": "Person",
+        "name": "vignesh",
+        "url": "https://vignesh.codes",
+        "sameAs": [
+          "https://github.com/Vigneshs9899",
+          "https://www.linkedin.com/in/vigneshs9899/"
+        ],
+        "jobTitle": "Fullstack Developer"
+      })
+    }}
+  />
     </head>
       <body
         className={`${outfit.className} ${ovo.className} antialiased leading-8 overflow-x-hidden dark:bg-darkTheme dark:text-white`}
