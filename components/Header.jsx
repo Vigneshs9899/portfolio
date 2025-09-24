6af0d01469176ceac4acf3a1e0896d1ce0f80b24
Vignesh <142200812+Vigneshs9899@users.noreply.github.com>
2025-09-24 18:25:38 +0530

    Update Header.jsx with new resume

--- components/Header.jsx
+++ components/Header.jsx
@@ -53,7 +53,7 @@ const Header = () => {
               transition={{ duration:0.3 }}
           
             
-            href="https://drive.google.com/file/d/1Ow78W-nCnmd9jrdJL2PXbkBEtxID-lGS/view?usp=sharing" download className='font-sans px-10 py-3 border rounded-full border-border flex items-center gap-2 bg-accent dark:text-textPrimary hover:bg-accentHover'>Download My resume </motion.a>
+            href="https://drive.google.com/file/d/1E52MHbstk7OE5ZQwnWoYs1qEtCiLtjt4/view?usp=sharing" download className='font-sans px-10 py-3 border rounded-full border-border flex items-center gap-2 bg-accent dark:text-textPrimary hover:bg-accentHover'>Download My resume </motion.a>
         </div>
 
     </div>
